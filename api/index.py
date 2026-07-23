ff0a2fc5aac56c99972e2eed0685c0f1f5f59396
FaganFeyzili <feyzilifeqan@gmail.com>
2026-07-23 10:27:32 +0000

    changed

--- api/index.py
+++ api/index.py
@@ -4,7 +4,7 @@ from .routes import get_news
 app = Flask(__name__)
 
 CORS(app, resources={r"/api/news": {
-    "origins": [],
+    "origins": ["https://faganfeyzili.github.io"],
     "methods": ["GET"]
 }})
 
